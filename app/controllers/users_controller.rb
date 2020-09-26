12dddbc830b205d12b44c37717eb193956c8c28a
jamesjlandry <james.j.landry@outlook.com>
2020-09-26 11:19:35 -0500

    fixed validation for admin login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
     def create
         user = User.create({username: params[:username], password: params[:password]})
+           if user.save
             session[:user_id] = user.id
             render json: user
+           else 
+            render json: nil 
+           end
     end
 end
